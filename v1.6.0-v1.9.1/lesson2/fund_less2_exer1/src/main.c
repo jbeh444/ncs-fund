f34e30466e3aa7efda8bd50560581c5073590942
jbeh444 <jaredbeh@mail.weber.edu>
2023-05-30 13:56:55 -0600

    fun

--- v1.6.0-v1.9.1/lesson2/fund_less2_exer1/src/main.c
+++ v1.6.0-v1.9.1/lesson2/fund_less2_exer1/src/main.c
@@ -2,50 +2,58 @@
  * Copyright (c) 2016 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
+ * Note: 
+ * Tested on nRF Connect SDK Version : 2.0
  */
 
-#include <zephyr.h>
-#include <device.h>
-#include <devicetree.h>
-#include <drivers/gpio.h>
-/* STEP 6 - Change the sleep time from 1000 ms to 100 ms */
-#define SLEEP_TIME_MS   1000
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
 
-/* STEP 3 - Define the symbol that will represent the button on the board*/
+/* STEP 7 - Change the sleep time from 1000 ms to 100 ms */
+#define SLEEP_TIME_MS   100
 
-/* The devicetree node identifier for the "led0" alias. */
+/* STEP 3.1 - Get the node identifier for button 1 through its alias sw0 */
+#define SW0_NODE	DT_ALIAS(sw0)
+
+/* STEP 3.2 - Get the device pointer. pin number, and pin's configuration flags through gpio_dt_spec */
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+
+/* LED0_NODE is the devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
-#define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
-#else
-/* A build error here means your board isn't set up to blink an LED. */
-#error "Unsupported board: led0 devicetree alias is not defined"
-#define LED0	""
-#define PIN	0
-#define FLAGS	0
-#endif
 
 void main(void)
 {
-	const struct device *dev;
 	int ret;
 
-	dev = device_get_binding(LED0);
-	if (dev == NULL) {
+	if (!device_is_ready(led.port)) {
+		return;
+	}
+	/* STEP 4 - Verify that the device is ready for use */
+	if (!device_is_ready(button.port)) {
 		return;
 	}
 
-	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return;
 	}
-	/* STEP 4 - Configure the pin connected to the button to be an input pin and set its hardware specifications */
 
+	/* STEP 5 - Configure the pin connected to the button to be an input pin and set its hardware specifications */
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret < 0) {
+		return;
+	}
 	while (1) {
-	/* STEP 5 - Read the status (pressed = 1 , unpressed = 0) of the button and update the LED accordingly */
+		/* STEP 6.1 - Read the status of the button and store it */
+        bool val = gpio_pin_get_dt(&button);
 
+		/* STEP 6.2 - Update the LED to the status of the button */
+        gpio_pin_set_dt(&led,val);
+
+        k_msleep(SLEEP_TIME_MS); // Put the main thread to sleep for 100ms for power optimization
 	}
 }
--- v2.x.x/lesson6/fund_less6_exer1_solution/src/main.c
+++ v2.x.x/lesson6/fund_less6_exer1_solution/src/main.c
@@ -5,72 +5,132 @@
  * 
  * Note: Tested on nRF52833 DK
  */
-
+//#include "arduino_bma456.h"
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
-/* STEP 3 - Include the header file of the I2C API */
 #include <zephyr/drivers/i2c.h>
-/* STEP 4.1 - Include the header file of printk() */
 #include <zephyr/sys/printk.h>
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#include "bma456.h"
+#include "bma4_defs.h"
+#include "bma4.h"
 
-/* STEP 8 - Define the I2C slave device address and the addresses of relevant registers */
-#define STTS751_TEMP_HIGH_REG            0x00
-#define STTS751_TEMP_LOW_REG             0x02
-#define STTS751_CONFIG_REG               0x03
+
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   500
 
 /* STEP 6 - Get the node identifier of the sensor */
 #define I2C_NODE DT_NODELABEL(mysensor)
 
 void main(void)
 {
+	struct bma4_dev bma = { 0 };
+	uint8_t ret;
+	uint8_t chip_ID;
+	uint8_t status_features = 0x2A;
 
-	int ret;
+	
+	ret = bma456_init(&bma);
 
 /* STEP 7 - Retrieve the API-specific device structure and make sure that the device is ready to use  */
 	static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C_NODE);
 	if (!device_is_ready(dev_i2c.bus)) {
 		printk("I2C bus %s is not ready!\n\r",dev_i2c.bus->name);
 		return;
-	}
-
+	}		
+	printk("Chip ID is %x \n", chip_ID);
 /* STEP 9 - Setup the sensor by writing the value 0x8C to the Configuration register */
-	uint8_t config[2] = {STTS751_CONFIG_REG,0x8C};
-	ret = i2c_write_dt(&dev_i2c, config, sizeof(config));
+	//uint8_t config[2] = {STTS751_CONFIG_REG,0x8C};
+	printk("I2C bus %s is ready!\n\r",dev_i2c.bus->name);
+	ret = i2c_write_dt(&dev_i2c, 0x00, sizeof(0x00));//set pointer to 0x00 in BMA456 to read chip ID		
+	ret = i2c_read_dt(&dev_i2c, &chip_ID, sizeof(chip_ID));	
 	if(ret != 0){
-		printk("Failed to write to I2C device address %x at Reg. %x \n", dev_i2c.addr,config[0]);
+		printk("Failed to read from I2C device address %x at Reg. %x n", dev_i2c.addr,chip_ID);
+	}
+	printk("Chip ID is %x \n", chip_ID);
+	if (chip_ID == 0x16){
+		bma.chip_id = chip_ID; //set chip ID in bma4_dev struct
+		printk("Chip ID is correct \n");
+	}
+	else{
+		printk("Chip ID is incorrect \n");
+	}
+	uint8_t disable_pwr[2] = {0x7C,0x00};
+	ret = i2c_write_dt(&dev_i2c, disable_pwr, sizeof(disable_pwr));
+	if(ret != 0){
+		printk("Failed to write to I2C device address %x at Reg. %x \n", dev_i2c.addr,disable_pwr[0]);
 		return;
 	}
-
-	while (1) {
-/* STEP 10 - Read the temperature from the sensor */
-		uint8_t temp_reading[2]= {0};
-		uint8_t sensor_regs[2] ={STTS751_TEMP_LOW_REG,STTS751_TEMP_HIGH_REG};
-		ret = i2c_write_read_dt(&dev_i2c,&sensor_regs[0],1,&temp_reading[0],1);
-		if(ret != 0){
-			printk("Failed to write/read I2C device address %x at Reg. %x \r\n", dev_i2c.addr,sensor_regs[0]);
-		}
-		ret = i2c_write_read_dt(&dev_i2c,&sensor_regs[1],1,&temp_reading[1],1);
-		if(ret != 0){
-			printk("Failed to write/read I2C device address %x at Reg. %x \r\n", dev_i2c.addr,sensor_regs[1]);
-		}
-
-/* STEP 11 - Convert the two bytes to a 12-bits */
-		int temp = ((int)temp_reading[1] * 256 + ((int)temp_reading[0] & 0xF0)) / 16;
-		if(temp > 2047)
-		{
-			temp -= 4096;
-		}
-
-		// Convert to engineering units 
-		double cTemp = temp * 0.0625;
-		double fTemp = cTemp * 1.8 + 32;
-
-		//Print reading to console  
-		printk("Temperature in Celsius : %.2f C \n", cTemp);
-		printk("Temperature in Fahrenheit : %.2f F \n", fTemp);
-		k_msleep(SLEEP_TIME_MS);
+	printk("Power down mode disabled \n");		
+	k_msleep(SLEEP_TIME_MS);
+	uint8_t prepare_features[2] = {0x59,0x00};
+	ret = i2c_write_dt(&dev_i2c, prepare_features, sizeof(prepare_features));
+	if(ret != 0){
+		printk("Failed to write to I2C device address %x at Reg. %x \n", dev_i2c.addr,prepare_features[0]);
+		return;
 	}
+	printk("Features prepared \n");
+	ret = bma456_write_config_file(&bma);
+	ret = i2c_burst_write_dt(&dev_i2c, 0x5E, *bma.config_file_ptr, sizeof(*bma.config_file_ptr));
+	if(ret != 0){
+		printk("Failed to write to I2C device address %x at Reg. \n", dev_i2c.addr);
+		return;
+	}
+	printk("Config file written \n");
+
+	uint8_t enable_features[2] = {0x59,0x01};
+	ret = i2c_write_dt(&dev_i2c, enable_features, sizeof(enable_features));	
+	if(ret != 0){
+		printk("Failed to write to I2C device address %x at Reg. %x \n", dev_i2c.addr,enable_features[0]);
+		return;
+	}
+	printk("Features enabled \n");
+	k_msleep(140);
+	
+	printk("Status Features %x \n", status_features);	
+	ret = i2c_read_dt(&dev_i2c, &status_features, sizeof(status_features));
+	if(ret != 0){
+		printk("Failed to read from I2C device address %x at Reg. %x \n", dev_i2c.addr,status_features);
+		return;
+	}
+	printk("Status Features %x \n", status_features);
+
+	printk("BMA456 is ready!\n\r");
+	printk("\n\r");
+
+	//ret = bma4_set_accel_enable(BMA4_ENABLE, &bma);
+	//ret = bma456_feature_enable(BMA456_STEP_CNTR, BMA4_ENABLE, &bma);
+	//ret = bma456_map_interrupt(BMA4_INTR1_MAP, BMA456_STEP_CNTR_INT, BMA4_ENABLE, &bma);
+// 	//ret = i2c_write_dt(&dev_i2c, 0x5E ,sizeof(0x5E)); 
+	//i2c_burst_write_dt(&dev_i2c, 0x5E, init_array , sizeof(init_array));
+// 	ret = i2c_write_dt(&dev_i2c,bma456_config_file,sizeof(bma456_config_file));
+// 	while (1) {
+// /* STEP 10 - Read the temperature from the sensor */
+// 		uint8_t temp_reading[2]= {0};
+// 		uint8_t sensor_regs[2] ={STTS751_TEMP_LOW_REG,STTS751_TEMP_HIGH_REG};
+// 		ret = i2c_write_read_dt(&dev_i2c,&sensor_regs[0],1,&temp_reading[0],1);
+// 		if(ret != 0){
+// 			printk("Failed to write/read I2C device address %x at Reg. %x \r\n", dev_i2c.addr,sensor_regs[0]);
+// 		}
+// 		ret = i2c_write_read_dt(&dev_i2c,&sensor_regs[1],1,&temp_reading[1],1);
+// 		if(ret != 0){
+// 			printk("Failed to write/read I2C device address %x at Reg. %x \r\n", dev_i2c.addr,sensor_regs[1]);
+// 		}
+
+// /* STEP 11 - Convert the two bytes to a 12-bits */
+// 		int temp = ((int)temp_reading[1] * 256 + ((int)temp_reading[0] & 0xF0)) / 16;
+// 		if(temp > 2047)
+// 		{
+// 			temp -= 4096;
+// 		}
+
+// 		// Convert to engineering units 
+// 		double cTemp = temp * 0.0625;
+// 		double fTemp = cTemp * 1.8 + 32;
+
+// 		//Print reading to console  
+// 		printk("Temperature in Celsius : %.2f C \n", cTemp);
+// 		printk("Temperature in Fahrenheit : %.2f F \n", fTemp);
+// 		k_msleep(SLEEP_TIME_MS);
+// 	}
 }
